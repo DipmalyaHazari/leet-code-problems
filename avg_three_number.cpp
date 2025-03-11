757a213f369fb83af9cee8f60edcb126c23ef79a
Dipmalya90 <dipmalyahazari2@gmail.com>
2025-03-11 13:55:09 +0530

    avg of three numbers in this problem

---
+++ avg_three_number.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    // Write C++ code here
+    int a=0,b=0,c=0;
+    std::cout<<"Enter the value of three number:";
+    std::cin>>a;
+    std::cin>>b;
+    std::cin>>c;
+    std::cout<<"avg of three number:"<<(a+b+c)/3;
+    return 0;
+}
