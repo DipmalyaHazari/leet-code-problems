3fdad3086bdf253e7fe60852db86347ad4172cb5
Dipmalya90 <dipmalyahazari2@gmail.com>
2025-03-11 14:19:33 +0530

    distance coversion prob

---
+++ distance_conversion.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main(){
+
+
+	double km=0,mil=0;
+
+	std::cout<<"enter the distance in km:";
+	std::cin>>km;
+	
+	mil=(1/1.6)*km;
+
+	std::cout<<"The distance in mil:";
+
+
+	return 0;
+
+}
+
